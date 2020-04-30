6e19ae8913dab48a7d7beaded16d4f8cfba13032
lingar <yimprogramming@gmail.com>
2020-04-30 13:08:17 +0300

    Showing event-error handling

--- src/new-work-on/error-handling.jsx
+++ src/new-work-on/error-handling.jsx
@@ -28,6 +28,9 @@ export function ErrorHandlingMain(props){
             <p>Sometime the things get into error and we should know how to handle it </p>
             <p>For example I have component on my app that if the data arrived from the server corrupted,
             even if it's only one of many good items, all the app fall down. </p>
+            <p>Important tip - when the screen out to error view and u want to return to the last
+            viewed screen press ESC (or u have an x also to close the error. Sometimes,
+            it's just layout for development mode, as will explained next ). </p>
 
             <img src= {error} alt = 'error image'/>
 
@@ -45,7 +48,8 @@ export function ErrorHandlingMain(props){
             <h2>Error boundary</h2>
             <p>After quick search you can found that
                <a href = "https://reactjs.org/docs/error-boundaries.html" target={'_blank'}> error-boundary </a>
-                functionality of React. </p>
+                functionality of React.<br/>
+            Live demo : https://codepen.io/gaearon/pen/wqvxGa?editors=0010</p>
 
             <p>This is the notes about that : </p>
             Note
@@ -127,9 +131,34 @@ export function ErrorHandlingMain(props){
 
             </p>
 
+            <h2>Handling event errors </h2>
+            <p>https://reactjs.org/docs/error-boundaries.html#how-about-event-handlers
+                <br/>
+                Error boundaries do not catch errors inside event handlers.
+
+                React doesn’t need error boundaries to recover from errors in event handlers. Unlike the render method and lifecycle methods, the event handlers don’t happen during rendering. So if they throw, React still knows what to display on the screen.
 
 
-            <p>Common mistakes : </p>
+            </p>
+            <div>
+                See this element and see how you click it's generate error even with the errorBoundary
+                (but works... - if the error is affecting rendering. If the out of
+                scope of rendering the boundaries wont help ):
+                <GeneralErrorHandling>
+                    <ElementUnHandledEvent data = {{name:"lingar"}} arr = {[]}/>
+                </GeneralErrorHandling>
+            </div>
+
+            <div>
+                This element is with handling  -
+                see how you click it's generate error evenand handle this:
+                <GeneralErrorHandling>
+                    <ElementHandledEvent data = {{name:"lingar"}} arr = {[]}/>
+                </GeneralErrorHandling>
+            </div>
+
+
+            <h2>Common mistakes : </h2>
             <ul>
                 <li>U forgot to implement the functions with the states. </li>
                 <li>U forgot to make the condition at the render </li>
@@ -302,6 +331,111 @@ class GeneralErrorHandling extends React.Component{
     }
 
 }
+
+class  ElementUnHandledEvent extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state ={
+            name: props.data.name,
+            arr: props.arr,
+            doError:false
+        }
+
+    }
+
+    generateError = ()=>{
+        console.log("Generating errors");
+        // this.setState({//this affects rendering so it's will do catch
+        //     arr:null
+        // })
+        throw new Error('I crashed!');//this not affect rendering so it won't be catch
+
+
+    }
+
+    render() {
+
+
+        return (
+            <div className={'container'}>
+                <h4>Here I am showing how to handle events error - that's shouldn't work with error boundaries .</h4>
+                I am  element . Name : {this.state.name+""}
+                <br/>
+                Array second element: {this.state.arr[1]+""}
+                <br/>
+                <button onClick={this.generateError}>Generate error </button>
+
+            </div>
+        )
+    }
+
+
+}
+
+class  ElementHandledEvent extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state ={
+            name: props.data.name,
+            arr: props.arr,
+            doError:false,
+            customErr: false
+        }
+
+    }
+
+    generateError = ()=>{
+        console.log("Generating errors");
+        // this.setState({//this affects rendering so it's will do catch
+        //     arr:null
+        // })
+        throw new Error('I crashed!');//this not affect rendering so it won't be catch
+
+
+    }
+    handleClick =() =>{
+        try{
+            this.generateError();
+        }catch (e) {
+            console.log("Error happened - ", e);
+            this.setState({
+                customErr: true
+            });
+        }
+    }
+
+    render() {
+
+        if(this.state.customErr){
+            return (
+                <div style = {{backgroundColor: 'pink', color: 'yellow', border: '2px solid white',display: 'inline-block', padding: '10px'}}>
+                    <p >Event error on this component </p>
+
+
+                </div>
+            );
+        }
+        return (
+            <div className={'container'}>
+                <h4>Here I am showing how to handle events error - that's shouldn't work with error boundaries .</h4>
+                I am  element . Name : {this.state.name+""}
+                <br/>
+                Array second element: {this.state.arr[1]+""}
+                <br/>
+                <button onClick={this.handleClick}>Generate error with handling </button>
+
+            </div>
+        )
+    }
+
+
+}
+
+
 
 function sideEffectFunc(data){
     console.log("I am sideEffectFunc(data) that shows from out params.. ")
