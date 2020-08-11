e025a5b99b0cdae82af2771fadc75d4e507829ad
lingar <yimprogramming@gmail.com>
2020-08-11 11:44:29 +0300

    Updated done, but hooks don't working inside classes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Example1} from "./new-work-on/react-hooks/sub-comps/DraftHooks";
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<Example1 />, document.getElementById('hooks'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/new-work-on/react-hooks/ReactHooksMain.jsx
+++ src/new-work-on/react-hooks/ReactHooksMain.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link, Route} from "react-router-dom";
-import {DraftHooks} from "./sub-comps/DraftHooks";
+import {DraftHooks, Example1} from "./sub-comps/DraftHooks";
 
 export class ReactHooksMain extends React.Component {
     constructor(props) {
@@ -9,12 +9,21 @@ export class ReactHooksMain extends React.Component {
     }
     componentDidMount() {
         console.log("ReactHooksMain did mount - " , this.props);
+        console.log(React.version);
     }
 
     render(){
         return(
-            <div className='container-2'>
+            <div className='container'>
                 <h1>React-Hooks</h1>
+                <h3>U can't use hooks inside class component, U need to make them on single tree which I need to understand,
+                Or to make workarounds like
+                    <a target={'blank'} href = "https://www.google.com/search?newwindow=1&sxsrf=ALeKk01YaafKVRJQuR2Jsg25n83sOUr_mg%3A1597133191130&ei=h1EyX8fEB4Ox8gLe2KGYAg&q=react+hooks+and+class+on+same+tree+example&oq=react+hooks+and+class+on+same+tree+example&gs_lcp=CgZwc3ktYWIQAzoRCC4QsQMQgwEQxwEQowIQkwI6DgguELEDEIMBEMcBEKMCOggIABCxAxCDAToCCAA6CAguELEDEIMBOggILhDHARCjAjoLCC4QxwEQrwEQkwI6AgguOgQIABBDOgQIABAKOgQILhAKOgoILhDHARCjAhAKOgcILhBDEJMCOgQILhBDOgcILhANEJMCOgQILhANOgoILhDHARCjAhANOgoILhDHARCvARANOgQIABANOgUIABCRAjoFCAAQsQM6CwguELEDEMcBEKMCOgcIABCxAxBDOgUILhCxAzoICAAQsQMQkQI6CggAELEDEIMBEEM6BggAEBYQHjoICAAQFhAKEB46CAghEBYQHRAeOgUIIRCgAToHCCEQChCgAToECCEQFVCo9hdYz9QYYIHZGGgIcAB4AIAB1AGIAes3kgEGMC40NC4zmAEAoAEBqgEHZ3dzLXdperABAMABAQ&sclient=psy-ab&ved=0ahUKEwiHpJDk2JLrAhWDmFwKHV5sCCMQ4dUDCAw&uact=5
+">
+                        here :
+                    </a>
+                </h3>
+                {/*<Example1/>*/}
                 <h2>This is the main page of react-hooks studying</h2>
 
                 <p><Link to = {this.props.match.url+'/draft'}>Link to draft</Link></p>
