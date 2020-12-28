26102774d144ee3bb6ff6dd4aae6f832570b8a94
lingar <yimprogramming@gmail.com>
2020-12-28 02:59:44 +0200

    Small fix

--- src/new-work-on/state-management/render-props-explaining/RenderPropsPoc.jsx
+++ src/new-work-on/state-management/render-props-explaining/RenderPropsPoc.jsx
@@ -12,7 +12,6 @@ export class RenderPropsPoc extends React.Component {
             <div className="explain">
                 <h2>State data sharing management in react </h2>
                 <p>Render props -
-
                     <a href="https://reactjs.org/docs/render-props.html" target="_blank">
                         from here
                     </a>
@@ -20,15 +19,15 @@ export class RenderPropsPoc extends React.Component {
                      Next time :
                      Do the Missions of the poc in the word doc.
 
-                     – 4 products - with prices changing – with 1 list uses it, and one side bar which show the currently most expensive .
+                     – 4 products - with prices changing –
+                     with
+                     1- 1 list uses it,
+                     2- and one side bar which show the currently most expensive .
 
                      **/}
-
-
                 </p>
-
                 <p>products: </p>
-
+                <ProductsData/>
 
             </div>
 
@@ -41,6 +40,8 @@ class ProductsData extends React.Component {
 
     constructor() {
         super();
+        this.interval = null;
+        this.seconds = 3;
         this.state = {
             book: 0,
             bread: 0,
@@ -48,8 +49,12 @@ class ProductsData extends React.Component {
             candle: 0
         }
     }
-    render(){
-        return(
+    componentDidMount() {
+        this.startTimer();
+    }
+
+    render() {
+        return (
             <div>
                 products:
                 <p>book = {this.state.book}</p>
@@ -58,5 +63,26 @@ class ProductsData extends React.Component {
             </div>
         );
     }
+
+    startTimer() {
+        this.interval = setInterval(()=>{
+                this.setState({
+                    book: this.getRandomInt(100),
+                    bread: this.getRandomInt(100),
+                    shoes: this.getRandomInt(100),
+                    candle: this.getRandomInt(100)
+                });
+                console.log("interval");
+        }
+
+
+
+        ,this.seconds * 1000);
+    }
+
+    getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
 
 }
