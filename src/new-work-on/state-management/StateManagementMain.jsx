0dcaa74b2b199d64bae24dbe184e3ff27f9a9ab9
lingar <yimprogramming@gmail.com>
2021-02-03 12:33:22 +0200

    Starting create products store

--- src/new-work-on/state-management/StateManagementMain.jsx
+++ src/new-work-on/state-management/StateManagementMain.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import {RenderPropsPoc} from "./render-props-explaining/RenderPropsPoc";
 import {ReactHooksMain} from "../react-hooks/ReactHooksMain";
+import {ReduxMainView} from "./redux/redux-main";
 
 export class StateManagementMain extends React.Component {
 
@@ -21,12 +22,20 @@ export class StateManagementMain extends React.Component {
                 <Router>
                     <div className="router-area">
                         <ol>
-                            <li>Like above in the <a href="../explain3/">managing state in parents</a> section
+                            <li>One way data-flow - Like above in the <a href="../explain3/">managing state in parents</a> section
                                 described. This is the basic option.
                             </li>
                             <li><Link to="state-management-react/render-props-explanation/">Render - props </Link></li>
+                            <li><Link to="">React hooks - TODO . U have example in the todos project.  </Link></li>
+                            <li><Link to="">HOC - TODO  </Link></li>
+
+                            <li><Link to="state-management-react/redux-explanation/">Redux - maybe the most common and expanded way</Link></li>
+
+
                         </ol>
                         <Route path="/state-management-react/render-props-explanation" component={RenderPropsPoc}/>
+                        <Route path="/state-management-react/redux-explanation/" component={ReduxMainView}/>
+
 
                     </div>
                 </Router>
