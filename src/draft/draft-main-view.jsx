8558ac805bed46f5adfebf35d6fe9725874195eb
lingar <yimprogramming@gmail.com>
2021-02-11 10:56:00 +0200

    Adding Draft Template

---
+++ src/draft/draft-main-view.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {RegularComp1} from "./general-comps";
+
+export function DraftMainView(){
+    return(
+        <div className= "draft-main-view">
+            <h2>Draft main-view</h2>
+            <p>The component reside inside src/draft</p>
+            <p>Do here as you wish</p>
+            <RegularComp1/>
+
+
+        </div>
+    )
+}
---
+++ src/draft/general-comps.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function RegularComp1(){
+    return(
+        <div>
+            <p>I am regular functional comp</p>
+        </div>
+    )
+}
