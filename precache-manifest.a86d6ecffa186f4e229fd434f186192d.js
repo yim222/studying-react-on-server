6d1d1906336989b4c22d3341a46cc5408696e086
lingar <yimprogramming@gmail.com>
2021-04-27 10:04:40 +0300

    Updates

---
+++ precache-manifest.a86d6ecffa186f4e229fd434f186192d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8553f02327fb138daed89dfd11a76f0",
+    "url": "/studying-react-on-server/index.html"
+  },
+  {
+    "revision": "243e0adc691b05aa4772",
+    "url": "/studying-react-on-server/static/css/main.611bf2e6.chunk.css"
+  },
+  {
+    "revision": "b11d09a3bf5ea929c842",
+    "url": "/studying-react-on-server/static/js/2.269bc604.chunk.js"
+  },
+  {
+    "revision": "0fef09f97c30ad25cb44221d280778ce",
+    "url": "/studying-react-on-server/static/js/2.269bc604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243e0adc691b05aa4772",
+    "url": "/studying-react-on-server/static/js/main.73527c20.chunk.js"
+  },
+  {
+    "revision": "96d9da84d544ae719a03",
+    "url": "/studying-react-on-server/static/js/runtime-main.58eaebb0.js"
+  },
+  {
+    "revision": "2ba40259d42852ad7f69917778bfb4ce",
+    "url": "/studying-react-on-server/static/media/error.2ba40259.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/studying-react-on-server/static/media/logo.5d5d9eef.svg"
+  }
+]);
