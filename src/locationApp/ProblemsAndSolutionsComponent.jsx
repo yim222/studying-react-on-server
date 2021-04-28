87d8898fe7a2ab2b617d611538d822fba59cf0bd
lingar <yimprogramming@gmail.com>
2021-04-28 09:24:48 +0300

    Making cors explanation

--- src/locationApp/ProblemsAndSolutionsComponent.jsx
+++ src/locationApp/ProblemsAndSolutionsComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function ProblemsAndSolutions() {
 
     return (
-        <div >
+        <div>
             <h2>Here introduce some problems with possible solutions (hopefully they have been solved)</h2>
             <RssCorsProblem/>
         </div>
@@ -19,37 +19,81 @@ function RssCorsProblem() {
     let proxy = "https://cors-anywhere.herokuapp.com/";//https://cors-anywhere.herokuapp.com/
     let fox = "http://feeds.foxnews.com/foxnews/national";
 
-    let request2 = new XMLHttpRequest();
+    let failedRequest = new XMLHttpRequest();
 
-    request2.onreadystatechange = () => {
-        if (request2.readyState == 4 && request2.status == 200) {
+    failedRequest.onreadystatechange = () => {
+        if (failedRequest.readyState == 4 && failedRequest.status == 200) {
             // let myObj3 = JSON.parse(request2.responseText);
             console.log("lingar res 3");
-            console.log(request2);
+            console.log(failedRequest);
             console.log("object??")
-            console.log(request2.responseText)
+            console.log(failedRequest.responseText)
             // console.log(myObj3)
+
+        }
+    }
+    //This request possibility will  be  failed , by cors error, or also http error. And maybe others.
+    failedRequest.open("GET", cnn);//fox/ynet/ cnn
+    // request2.open("GET", proxy +  cnn);//fox/ynet/ cnn -- this will fix it
+
+    failedRequest.send();
+
+
+    let goodRequest = new XMLHttpRequest();
+
+    goodRequest.onreadystatechange = () => {
+        if (goodRequest.readyState == 4 && goodRequest.status == 200) {
+            console.log("good reuqest");
+            console.log(goodRequest);
+            console.log(goodRequest.responseText)
 
         }
 
     }
 
-    request2.open("GET", proxy + cnn);//fox/ynet/ cnn
+    //This will be working by using the proxy  https://cors-anywhere.herokuapp.com/
+    //in the first time you run it, it will also fail. U need to request temp access. See the docs there and on the web on how
+    //to create proxy by your self.
+    goodRequest.open("GET", proxy + cnn);//fox/ynet/ cnn
     // request2.open("GET",  cnn);//fox/ynet/ cnn
 
-    request2.send();
+    goodRequest.send();
 
     return (
         <div className="app-body">
             <h3>
                 I have this annoying problem - when I want to get RSS it's blocked by Cors-origin.
             </h3>
+
+            <h4>Problem (See below long explanation)</h4>
+            <p> open the log and see how failedRequest is failing.</p>
+            <p>It's yelling about ??? -CORS!</p>
+            <p>request ... has been blocked by CORS policy: ... (see the logs F12) </p>
+            <p>This request possibility will be failed , by cors error, or also http error. And maybe others.
+            </p>
+            <h4>Solution... </h4>
+            <p>First U need to go to <a href="https://cors-anywhere.herokuapp.com/"
+                                        target="_blank">https://cors-anywhere.herokuapp.com/</a>
+                and access temporary access (before doing that - U will get "forbidden error"
+                on the goodRequest too. </p>
+            <p>See good request - it's working... </p>
+            <p>U can see the xml data in the logs. </p>
+            <p>This will be working by using the proxy
+                https://cors-anywhere.herokuapp.com/
+                in the first time you run it, it will also fail. U need to request temp access. See the docs there and
+                on the web on how
+                to create proxy by your self.</p>
+
+
+            <h4>Long explanation </h4>
             <p>By what I've read until now, I'm understand that this is restriction by browser. You cannot affect it
                 from the code directly since it's depend on the response - when the response is returning it's already
                 have the problematic
                 thing that prevent the browser from get it. This is the content: </p>
             <p>
-                Access to XMLHttpRequest at 'http://rss.cnn.com/rss/edition.rss' from origin 'http://localhost:3007' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+                Access to XMLHttpRequest at 'http://rss.cnn.com/rss/edition.rss' from origin 'http://localhost:3007' has
+                been blocked by CORS policy: Response to preflight request doesn't pass access control check: No
+                'Access-Control-Allow-Origin' header is present on the requested resource.
 
             </p>
 
@@ -66,12 +110,6 @@ function RssCorsProblem() {
                 ability to say they want to opt into allowing cross-origin access to their resources.</p>
             <p>There are some possible workarounds </p>
 
-            <h4>Problem one - not working. </h4>
-            <p> see in the code that request 1 not working</p>
-        {/*    Todo -
-            - Make that the request will have a status.
-
-        */}
 
         </div>
     )
